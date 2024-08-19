db793e94f247075122a06cfc29e6c2778d2f364f
Fabio Alves <fabio_alvss@outlook.com>
2024-08-19 20:34:45 -0300

    Adição de arquivos importantes para o projeto

---
+++ SpringBoot/clientRegister/src/main/java/com/newtech/clientRegister/ClientRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.newtech.clientRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientRegisterApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/clientRegister/src/test/java/com/newtech/clientRegister/ClientRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newtech.clientRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
